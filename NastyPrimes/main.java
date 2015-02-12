1ddb7322515f5d255fb39c54542abb09172e2ef4
technicalities <technicalities@hotmail.co.uk>
2015-02-12 18:16:39 +0000

    Added the worst prime number calculator possible.
    Eh. Shows off SwingWorker.

---
+++ NastyPrimes/main.java
@@ -0,0 +1,16 @@
+import javax.swing.SwingUtilities;
+
+public class main{
+	
+
+public static void main(String[] args) 
+{
+	SwingUtilities.invokeLater(new Runnable() 
+	{
+		public void run() 
+		{
+			new Primes();
+		}
+	});
+}
+}
