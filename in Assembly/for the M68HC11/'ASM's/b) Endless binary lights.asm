c24c48818b2829ca8c5e4220c3db04b7368d0a8e
technicalities <technicalities@users.noreply.github.com>
2015-09-18 11:50:14 +0100

    Update b) Endless binary lights.asm

--- in Assembly/for the M68HC11/'ASM's/b) Endless binary lights.asm
+++ in Assembly/for the M68HC11/'ASM's/b) Endless binary lights.asm
@@ -1,5 +1,5 @@
 *  "Use the I/O Box LEDs to count forever through the binary sequence
-*  0000 0000 --> 0000 0001 --> ... --> 1111 1111 --> 0000 0000 --> 0000 0001 ...
+*  0000 0000 --> 0000 0001 --> ... --> 1111 1111 --> 0000 0000 --> 0000 0001 ..."
 
 *  A program that counts with 8 binary LEDs, overflowing back to '0' from '255'. It does this by 
 *  the following extremely simple: increment a register, then translate the new count to LED binary in a loop.
